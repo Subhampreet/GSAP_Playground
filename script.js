291d2949e884d73e66e18426d6d2093403b7346e
Subhampreet <subhampreet18@gmail.com>
2024-05-26 13:28:01 +0530

    Text Marquee Build 1.0.0

--- script.js
+++ script.js
@@ -69,7 +69,42 @@ sectionOneAnimation()
 //     }
 // })
 
+let currentScroll = 0;
+let isScrollingDown = true;
+let arrows = document.querySelectorAll(".arrow");
 
+let tween = gsap.to(".marquee_part", {
+    xPercent: -100,
+    repeat: -1,
+    duration: 5, 
+    ease: "linear"
+}).totalProgress(0.5);
+
+
+gsap.set("marquee_inner", {xPercent: -50});
+
+window.addEventListener("scroll", function() {
+
+    if(this.window.pageYOffset > currentScroll){
+        isScrollingDown = true;
+    } else {
+        isScrollingDown = false;
+    }
+
+    gsap.to(tween, {
+        timeScale: isScrollingDown ? 1 : -1,    
+    });
+
+    arrows.forEach((arrow) => {
+        if(isScrollingDown) {
+            arrow.classList.remove("active");
+        } else {
+            arrow.classList.add("active");
+        }
+    });
+
+    currentScroll = window.pageYOffset;
+})
 
 
 
